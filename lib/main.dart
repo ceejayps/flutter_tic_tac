8b0ef165c38c39d7c20d3dd666f08eeda45400bf
ceejayps <59537158+ceejayps@users.noreply.github.com>
2022-01-13 01:19:08 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tic_tac_toe_example/utils.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
